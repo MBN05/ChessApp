6c8cdf5287b1e74e2c3ea48cd30055008ae7e487
MBN05 <nehamb@student.tce.edu>
2025-10-17 12:31:27 +0530

    Set backend URL for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://chessapp-odok.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
